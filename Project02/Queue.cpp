0fd16053cd859f29d40e04aef47bc34caf5bfe7e
bgtttttt <vladislav.navitski@gmail.com>
2024-04-30 22:48:05 +0300

    Queue

--- Project02/Queue.cpp
+++ Project02/Queue.cpp
@@ -1,1 +1,53 @@
 #include "Queue.h"
+
+int Queue::getSize() {
+	return size;
+};
+bool Queue::isEmpty() {
+	return size == 0;
+};
+void Queue::enqueue(int element) {
+	if (isEmpty()) {
+		queue = new int[1];
+		queue[0] = element;
+	}
+	else {
+		int* temp = new int[size + 1];
+		for (int i = 0; i < size; i++) {
+			temp[i] = queue[i];
+		}
+		temp[size] = element;
+		delete[] queue;
+		queue = temp;
+	}
+	size++;
+};
+int Queue::dequeue() {
+	if (!isEmpty()) {
+		int* temp = new int[size - 1];
+
+		int element = queue[0];
+		for (int i = 0; i < size-1; i++) {
+			temp[i] = queue[i+1];
+		}
+		size--;
+		delete[] queue;
+		queue = temp;
+		return element;
+	}
+	return 0;
+};
+int Queue::peek() {
+	if (!isEmpty()) {
+		return queue[0];
+	}
+
+	return 0;
+};
+string Queue::getInfo() {
+	string s = "Queue: ";
+	for (int i = size - 1; i >= 0; i++) {
+		s += to_string(queue[i]) + " ";
+	}
+	return s;
+};
--- Project02/Queue.h
+++ Project02/Queue.h
@@ -1,14 +1,20 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Queue
 {
 private:
 	int* queue;
-	int size;
+	int size{ 0 };
 public:
 	void enqueue(int element);
 	int dequeue();
 	int peek();
 	int getSize();
 	bool isEmpty();
+	string getInfo();
 };
 
